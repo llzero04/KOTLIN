1be9878e7cc5160be0635914d777cf50709189e2
llzero04 <sm.sanchith04@gmail.com>
2021-06-16 22:07:05 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/classprojects/QuizApp.kt
@@ -0,0 +1,31 @@
+package com.example.classprojects
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class QuizApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_app)
+
+        var btnQuiz = findViewById<Button>(R.id.btnQuiz);
+        var etUserName = findViewById<EditText>(R.id.etUserName);
+
+        btnQuiz.setOnClickListener {
+            if(etUserName.text.toString().isEmpty())
+            {
+                Toast.makeText(this , "Enter User Name" , Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                var next : Intent = Intent(this , StartQuiz()::class.java);
+                startActivity(next);
+//                finish();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/classprojects/StartQuiz.kt
@@ -0,0 +1,13 @@
+package com.example.classprojects
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartQuiz : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_quiz)
+
+
+    }
+}
